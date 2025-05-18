5e91545ba44c9baa099db24531cf5880c38eb9e6
Thiago Ribeiro <thiago.escola321@gmail.com>
2025-05-18 16:50:01 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+string respostaMover;
+string respostaDeveria;
+
+Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+respostaMover = Console.ReadLine()!.ToUpper();
+
+Console.Write("Deveria (S/N)? ");
+respostaDeveria = Console.ReadLine()!.ToUpper();
+
+if (respostaMover == "S" && respostaDeveria == "S")
+{
+    Console.WriteLine("Otímo");
+}
+
+else if (respostaMover == "S" && respostaDeveria == "N")
+{
+    Console.WriteLine("Use Silver Tape.");
+}
+
+else if (respostaMover == "N" && respostaDeveria == "S")
+{
+    Console.WriteLine("Use WD-40.");
+}
+
+else if (respostaMover == "N" && respostaDeveria == "N")
+{
+    Console.WriteLine("Otímo");
+}
+else
+{
+    Console.WriteLine("Resposta inválida.");
+}
+
+
